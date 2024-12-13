727c7e07b4a5ccc088a3f494de46080df11784b8
PHPPress Framework <terabytefrelance@gmail.com>
2024-12-13 07:05:45 -0300

    Add Message enum to represent exception messages for helper classes

---
+++ src/Helper/Exception/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPPress\Helper\Exception;
+
+/**
+ * Represents the exception message for the helper classes.
+ *
+ * @copyright Copyright (C) 2024 PHPPress.
+ * @license GNU General Public License version 3 or later {@see LICENSE}
+ */
+enum Message: string
+{
+    case GETTING_UNKNOWN_PROPERTY = 'Getting unknown property: %s::%s.';
+    case GETTING_WRITE_ONLY_PROPERTY = 'Getting write-only property: %s::%s.';
+    case SETTING_UNKNOWN_PROPERTY = 'Setting unknown property: %s::%s.';
+    case SETTING_READ_ONLY_PROPERTY = 'Setting read-only property: %s::%s.';
+    case UNSETTING_READ_ONLY_PROPERTY = 'Unsetting read-only property: %s::%s.';
+    case UNSETTING_UNKNOWN_PROPERTY = 'Unsetting unknown property: %s::%s.';
+
+    public function getMessage(string ...$argument): string
+    {
+        return match ($this) {
+            self::GETTING_UNKNOWN_PROPERTY,
+            self::GETTING_WRITE_ONLY_PROPERTY,
+            self::SETTING_UNKNOWN_PROPERTY,
+            self::SETTING_READ_ONLY_PROPERTY,
+            self::UNSETTING_READ_ONLY_PROPERTY,
+            self::UNSETTING_UNKNOWN_PROPERTY => sprintf($this->value, ...$argument),
+        };
+    }
+}
