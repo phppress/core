1f3236d46953bf9c5cc655860283dfa27db597da
PHPPress Framework <terabytefrelance@gmail.com>
2024-12-13 18:34:00 -0300

    Add new error messages for inaccessible and missing methods in Message enum.

--- src/Di/Exception/Message.php
+++ src/Di/Exception/Message.php
@@ -18,6 +18,8 @@ enum Message: string
     case DEFINITION_TYPE_UNSUPPORTED = 'Unsupported definition type for "%s".';
     case DEPENDENCIES_IDX_NAME_POSITION = 'Dependencies indexed by name and by position in the same array are not allowed.';
     case INSTANTIATION_FAILED = 'Failed to instantiate component or class: "%s".';
+    case METHOD_NOT_ACCESSIBLE = 'Method "%s" in class "%s" is not publicly accessible.';
+    case METHOD_NOT_FOUND = 'Method "%s" not found in class "%s".';
     case PARAMETER_MISSING = 'Missing required parameter "%s" when calling "%s".';
 
     public function getMessage(string ...$argument): string
@@ -29,6 +31,8 @@ enum Message: string
             self::DEFINITION_INVALID,
             self::DEFINITION_TYPE_UNSUPPORTED,
             self::INSTANTIATION_FAILED,
+            self::METHOD_NOT_ACCESSIBLE,
+            self::METHOD_NOT_FOUND,
             self::PARAMETER_MISSING => sprintf($this->value, ...$argument),
         };
     }
